73e823c42848348e234dc77fdd2d9a84a9304e01
张双意 <zhangdan@zhangdandeMacBook-Air.local>
2022-12-12 14:07:43 +0800

    21题

--- 21-mergeTwoLists.js
+++ 21-mergeTwoLists.js
@@ -1,3 +1,19 @@
-function mergeTwoList (l1, l2) {
+var mergeTwoLists = function(l1, l2) {
+    let current = new ListNode(0);
+    let point = current;
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            point.next = l1
+            l1 = l1.next
+        } else {
+            point.next = l2
+            l2 = l2.next
+        }
 
-}
+        point = point.next
+    }
+
+    point.next = l1?l1 : l2
+    
+    return current.next;
+};
